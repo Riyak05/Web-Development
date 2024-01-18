b413c471b78a6fef78083cfebf0e2c09dfc73a36
Riyak05 <143267215+Riyak05@users.noreply.github.com>
2024-01-18 22:47:29 +0530

    Add files via upload

---
+++ Assignment_2/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
